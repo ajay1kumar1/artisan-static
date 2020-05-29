ef9d9acb9e421ccb7ddf5176bef4df54d9696f7c
aksingh2 <aksingh2@celegence.com>
2020-05-29 15:45:50 +0530

    demo

---
+++ config.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => 'https://ajay1kumar1.netlify.app',
+    'site' => [
+        'title' => 'My Blog',
+        'description' => 'Personal blog of Aj.',
+        'image' => 'default-share.png',
+    ],
+    'owner' => [
+        'name' => 'Ajay SR',
+        'twitter' => 'ajay1kumar1',
+        'github' => 'ajay1kumar1',
+    ],
+    'services' => [
+        'cmsVersion' => '2.10.49',
+        'analytics' => 'UA-XXXXX-Y',
+        'disqus' => 'artisanstatic',
+        'formcarry' => 'XXXXXXXXXXXX',
+        'cloudinary' => [
+            'cloudName' => 'artisanstatic',
+            'apiKey' => '365895137117119',
+        ],
+    ],
+    'collections' => [
+        'posts' => [
+            'path' => 'posts/{filename}',
+            'sort' => '-date',
+            'extends' => '_layouts.post',
+            'section' => 'postContent',
+            'isPost' => true,
+            'comments' => true,
+            'tags' => [],
+            'hasTag' => function ($page, $tag) {
+                return collect($page->tags)->contains($tag);
+            },
+            'prettyDate' => function ($page, $format = 'M j, Y') {
+                return date($format, $page->date);
+            },
+        ],
+        'tags' => [
+            'path' => 'tags/{filename}',
+            'extends' => '_layouts.tag',
+            'section' => '',
+            'name' => function ($page) {
+                return $page->getFilename();
+            },
+        ],
+    ],
+];
